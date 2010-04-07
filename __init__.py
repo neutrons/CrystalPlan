00f84b87b36ff22461b7475e329b171e73848d87
Janik Zikovsky <jzkovsky@hotmal.com>
2010-04-07 21:04:40 +0000

    Files added to separate GUI and model packages

---
+++ __init__.py
@@ -0,0 +1,7 @@
+"""CrystalPlan package file."""
+# Author: Janik Zikovsky, zikovskyjl@ornl.gov
+# Version: $Id: CrystalPlan.py 1127 2010-04-01 19:28:43Z 8oz $
+
+import gui
+import model
+from CrystalPlan_version import version as __version__
---
+++ gui/__init__.py
@@ -0,0 +1,9 @@
+"""__init__.py module setup for the CrystalPlan GUI code."""
+# Author: Janik Zikovsky, zikovskyjl@ornl.gov
+# Version: $Id: __init__.py 1127 2010-04-01 19:28:43Z 8oz $
+
+#We import the most important modules only.
+import frame_main
+import frame_qspace_view
+import display_thread
+#import launch_gui
